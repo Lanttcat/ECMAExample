4e76d0d409adaf5bd2e8681954009721f70ad7ed
NikoManiac <niko.sky.lan@gmail.com>
2019-03-19 19:57:22 +0800

    support high light code basic

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 // read js example and convert to html
-const fs = require('fs');
-var rimraf = require("rimraf");
+const fs = require('fs-extra');
+const rimraf = require("rimraf");
 
 const basePath = process.cwd();
 const exampleDirPath = `${basePath}/example/`;
@@ -19,6 +19,15 @@ function renderIndexHTML(features) {
   return features
     .map((({name}) => `<p><a href="/ECMAExample/examples/${name}.html">${name.replace(/_/, " ")}</a></p>`))
     .join("\n");
+}
+
+async function copyHighLightPlugin() {
+    try {
+        await fs.copy(`${basePath}/libs`, `${docsDir}libs`);
+        console.log('Copy highLight plugin success!')
+    } catch (err) {
+        console.error(err)
+    }
 }
 
 const examplesHTML = examples.map((example) => {
@@ -44,4 +53,5 @@ examplesHTML.forEach((html) => {
 
 fs.writeFileSync(`${docsDir}index.html`, indexPageTemplateStr.replace(/{content}/g, renderIndexHTML(examplesHTML)));
 
+copyHighLightPlugin();
 
